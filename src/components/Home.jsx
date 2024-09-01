392b425bc4787e8521f85b9f6abae97593e50ddc
Avinash <avinashkmg1310@gmail.com>
2024-09-02 05:18:54 +0530

    +Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
 
+const tourData = [
+	{
+		date: 'JUL 16',
+		city: 'DETROIT, MI',
+		venue: 'DTE ENERGY MUSIC THEATRE',
+	},
+	{
+		date: 'JUL 19',
+		city: 'TORONTO,ON',
+		venue: 'BUDWEISER STAGE',
+	},
+	{
+		date: 'JUL 22',
+		city: 'BRISTOW, VA',
+		venue: 'JIGGY LUBE LIVE',
+	},
+	{
+		date: 'JUL 29',
+		city: 'PHOENIX, AZ',
+		venue: 'AK-CHIN PAVILION',
+	},
+	{
+		date: 'AUG 2',
+		city: 'LAS VEGAS, NV',
+		venue: 'T-MOBILE ARENA',
+	},
+	{
+		date: 'AUG 7',
+		city: 'CONCORD, CA',
+		venue: 'CONCORD PAVILION',
+	},
+];
+
 const Home = () => {
-	return <div>Home Page</div>;
+	return (
+		<section className="pb-32 px-6 max-w-[900px] my-0 mx-auto text-lg">
+			<h2 className="text-5xl font-bold text-center capitalize p-5 font-serif">TOURS</h2>
+			<div>
+				{tourData.map((tour, i) => {
+					return (
+						<div className="flex p-2 border-b border-gray-900 text-[#777] font-semibold text-wrap">
+							<span className=" w-[12%] text-gray-900 font-bold">{tour.date}</span>
+							<span className="w-1/4">{tour.city}</span>
+							<span className="w-[53%]">{tour.venue}</span>
+							<button className="w-1/5 bg-sky-500 p-2 rounded-md font-bold text-white/90 hover:bg-sky-600">
+								BUY TICKETS
+							</button>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Home;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 	const cartBtn = useLocation();
 
 	return (
-		<header className="bg-gray-300 flex flex-col relative w-full">
+		<header className="bg-[dimgray] flex flex-col relative w-full">
 			<div className="  bg-black text-white p-4 flex w-full fixed top-0 z-10">
 				<ul
 					className={` flex flex-1 justify-center items-center gap-20 pl-24 font-semibold max-sm:text-sm max-sm:gap-6 max-sm:pl-16 max-xs:justify-start max-xs:pl-0 ${
@@ -27,9 +27,22 @@ const Header = () => {
 				{cartBtn.pathname === '/' && <HeaderCartBtn />}
 			</div>
 
-			<h1 className="text-6xl text-center font-bold  pt-20 pb-4 bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-red-500 max-xs:text-4xl">
-				The Generics
-			</h1>
+			<div className="pt-20 pb-8 flex flex-col items-center">
+				<h1 className="text-6xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-red-500 max-xs:text-4xl">
+					The Generics
+				</h1>
+
+				{cartBtn.pathname === '/home' && (
+					<div className="mt-10 py-5 text-white/80 flex flex-col items-center gap-5">
+						<button className=" block bg-black/35 hover:bg-black/65 border-2 border-[#56CCF2] border-opacity-60 hover:border-opacity-90 px-4 py-3 rounded text-2xl font-medium">
+							Get our Latest Album
+						</button>
+						<button className="block bg-black/35 hover:bg-black/60 border-2 border-[#56CCF2] border-opacity-60 hover:border-opacity-90 rounded-full text-[#56CCF2] text-3xl font-black px-6 py-5">
+							►
+						</button>
+					</div>
+				)}
+			</div>
 		</header>
 	);
 };
