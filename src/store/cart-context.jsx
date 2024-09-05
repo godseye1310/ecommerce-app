3f57fabe4e55aa2d9ca538a849612356362584d6
Avinash <avinashkmg1310@gmail.com>
2024-09-05 15:33:18 +0530

    userCart "_"

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -1,22 +1,43 @@
 import React, { useContext, useState } from 'react';
+import axios from 'axios';
 
 export const CartData = React.createContext();
+
+const API_URL = 'https://crudcrud.com/api/f2e5e17aa46b4f3e9353bee15071337e/cart';
 
 export const CartProvider = ({ children }) => {
 	const [cart, setCart] = useState([]);
 	const [total, setTotal] = useState(0);
+	const [mail, setMail] = useState();
+	// const [cleanedEmail, setCleanedEmail] = useState('');
 
-	const addCartItem = (item) => {
+	// const cleanMail = mail.replace(/[@.]/g, '');
+
+	// Remove '@' and '.'
+
+	const addCartItem = async (item) => {
 		// console.log(item);
-		setCart((prevCart) => {
-			for (let preItem of prevCart) {
-				if (preItem.id === item.id) {
-					preItem.quantity += item.quantity;
-					return [...prevCart];
-				}
-			}
-			return [...prevCart, item];
-		});
+		// setCart((prevCart) => {
+		// 	for (let preItem of prevCart) {
+		// 		if (preItem.id === item.id) {
+		// 			preItem.quantity += item.quantity;
+		// 			return [...prevCart];
+		// 		}
+		// 	}
+		// 	return [...prevCart, item];
+		// });
+
+		const cleanMail = await mail.replace(/[@.]/g, '');
+		console.log(cleanMail);
+
+		try {
+			const response = await axios.post(`${API_URL}${cleanMail}`, cart);
+			console.log(response.data);
+			// console.log(response.status, response.statusText, 'List POST Success');
+			setCart((prevCart) => [...prevCart, response.data]);
+		} catch (error) {
+			console.log(error);
+		}
 
 		setTotal((prevTotal) => prevTotal + item.quantity * item.price);
 	};
@@ -35,11 +56,24 @@ export const CartProvider = ({ children }) => {
 		});
 	};
 
+	const loginCartHandle = async () => {
+		try {
+			const response = await axios.get(`${API_URL}/gilmailcom`);
+			console.log('Added', response.data);
+			console.log(response.status, response.statusText, 'Fetch on Refresh Success');
+			setCart(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	const cartctx = {
 		addCartItem,
 		removeCartItem,
 		cart,
 		total,
+		setMail,
+		loginCartHandle,
 	};
 
 	return <CartData.Provider value={cartctx}>{children}</CartData.Provider>;
