4ce4daaa1d60e8fa954d6558642f486c9d6ecb25
Avinash <avinashkmg1310@gmail.com>
2024-09-08 23:12:33 +0530

    fetch on userCart on page refresh/login

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -16,33 +16,54 @@ export const CartProvider = ({ children }) => {
 	const { isLoggedIn, token, userMail } = useAuth();
 
 	if (isLoggedIn) {
-		cleanMail = userMail.split(/[@.]/).join('');
+		cleanMail = `cartOf${userMail.split(/[@.]/).join('')}`;
 	}
 
 	const addCartItem = async (item) => {
 		console.log(item);
 		try {
-			const response = await axios.post(`${API_URL}/cartOf${cleanMail}.json?auth=${token}`, item);
-			console.log(response);
+			const response = await axios.post(`${API_URL}/${cleanMail}.json?auth=${token}`, item);
+			// console.log(response);
+			console.log(response.status, response.statusText, 'Item POST Success');
+			// Update Cart State...
 			setCart((prevCart) => [...prevCart, { ...item, id: response.data.name }]);
 		} catch (error) {
 			console.log(error);
 			console.log(error.message);
 		}
-		// const cleanMail = await mail.replace(/[@.]/g, '');
-		// console.log(cleanMail);
-
-		// try {
-		// const response = await axios.post(`${API_URL}${cleanMail}`, cart);
-		// console.log(response.data);
-		// console.log(response.status, response.statusText, 'List POST Success');
-		// setCart((prevCart) => [...prevCart, item]);
-		// }
 		setTotal((prevTotal) => prevTotal + item.quantity * item.price);
 	};
 
+	// Set Cart Sate on UserLogin OR Preserve cart State on refresh...
+	useEffect(() => {
+		const fetchUserCart = async () => {
+			if (token && isLoggedIn) {
+				try {
+					const response = await axios.get(`${API_URL}/${cleanMail}.json?auth=${token}`);
+					if (response.status === 200) {
+						const loggedInUserCart = Object.keys(response.data).map((key) => {
+							return { ...response.data[key], id: key.toString() };
+						});
+						console.log(loggedInUserCart);
+						setCart(loggedInUserCart);
+						setTotal(() => {
+							return loggedInUserCart.reduce((acc, curr) => {
+								return acc + curr.quantity * curr.price;
+							}, 0);
+						});
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		};
+		if (isLoggedIn) {
+			fetchUserCart();
+		}
+	}, [isLoggedIn, token]);
+
 	// console.log(cart);
-	// console.log(total);
+	console.log(total);
 	const removeCartItem = (id) => {
 		setCart((prevCart) => {
 			return prevCart.filter((item) => item.id !== id);
@@ -55,8 +76,6 @@ export const CartProvider = ({ children }) => {
 			}
 		});
 	};
-
-	useEffect(() => {});
 
 	// const loginCartHandle = async () => {
 	// 	try {
